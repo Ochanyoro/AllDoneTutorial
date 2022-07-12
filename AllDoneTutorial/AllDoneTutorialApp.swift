cf7af6d7f144e67f706f2d10c7735471530e4fc2
Ochanyoro <wadaoo0223@icloud.com>
2022-07-12 14:31:20 +0900

    Initial Commit

---
+++ AllDoneTutorial/AllDoneTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneTutorialApp.swift
+//  AllDoneTutorial
+//
+//  Created by 大和田一裕 on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
